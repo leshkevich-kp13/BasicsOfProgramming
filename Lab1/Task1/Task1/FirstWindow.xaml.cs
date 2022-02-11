c27e80808c9881545c68ef29ac09fd77da65eaa3
ads-leshkevich-kp13 <kp.leshkevich@gmail.com>
2022-02-11 21:00:14 +0200

    Lab №1

---
+++ Lab1/Task1/Task1/FirstWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+    public partial class FirstWindow : Window
+    {
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveToMainFromFirstBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            Hide();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Lab1/Task1/Task1/WinnerMessage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+
+    public partial class WinnerMessage : Window
+    {
+        public WinnerMessage(string message)
+        {
+            InitializeComponent();
+            Message_Label.Content = message;
+        }
+
+        private void OK_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
